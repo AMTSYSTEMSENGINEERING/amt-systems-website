dc8ef059730702668d81d8141080ce51ce3ebc5a
tiojio romain junior <tiojioromain02@gmail.com>
2025-07-24 20:23:22 +0200

    deploy test app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './assets/log.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          website AMT Systems
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="txt">AMT Systems Engeneering</h1>
       </header>
     </div>
   );
